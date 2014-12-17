886c472c744bcb37cad2e565ac5c9a0514e02eb7
danWatkinson <dan.watkinson@gmail.com>
2014-12-17 13:51:15 +0000

    stick a main on it; make sure my mocking matches reality...
    (I'm just doing right-click->run in IntelliJ, "java RockPaperScissorsGame" from a commandline should do it)

--- src/RockPaperScissorsGame.java
+++ src/RockPaperScissorsGame.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 /**
@@ -8,6 +9,11 @@ import java.io.PrintStream;
 public class RockPaperScissorsGame {
     private final BufferedReader in;
     private final PrintStream out;
+
+    public static void main(String[] args) {
+        new RockPaperScissorsGame(new BufferedReader(new InputStreamReader(System.in)), System.out)
+                .start();
+    }
 
     public RockPaperScissorsGame(final BufferedReader in, final PrintStream out) {
         this.in = in;
