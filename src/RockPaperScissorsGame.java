b6a082c6a20b443c559f5288c53f8e3913720f37
danWatkinson <dan.watkinson@gmail.com>
2014-12-17 16:01:05 +0000

    refactoring; cleaning the start method to what it needs to be

--- src/RockPaperScissorsGame.java
+++ src/RockPaperScissorsGame.java
@@ -18,24 +18,91 @@ public class RockPaperScissorsGame {
     }
 
     public void start() {
+        Selection playersSelection = getSelection();
+        out.println("You selected " + playersSelection);
+    }
+
+    private Selection getSelection() {
         out.println("Please select 'Rock', 'Paper', or 'Scissors'");
-        try {
-            boolean gotSomeValidInput = false;
-            while (!gotSomeValidInput) {
-                String input = in.readLine();
-                if (isValidInput(input)) {
-                    gotSomeValidInput = true;
-                    out.println("You selected " + input);
-                } else {
+        SelectionBuilder selectionBuilder = new SelectionBuilder();
+        Selection selection = new InvalidSelection();
+
+        while (!selection.isValid()) {
+            try {
+                selection = selectionBuilder.parse(in.readLine());
+                if (!selection.isValid()) {
                     out.println("Sorry, I didn't recognise that. Please try again: Rock Paper or Scissors");
                 }
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to read input.", e);
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to read input.", e);
+        }
+        return selection;
+    }
+
+    private class SelectionBuilder {
+        public Selection parse(final String input) {
+            if ("Rock".equals(input)) {
+                return new Rock();
+            } else if ("Paper".equals(input)) {
+                return new Paper();
+            } else if ("Scissors".equals(input)) {
+                return new Scissors();
+            } else {
+                return new InvalidSelection();
+            }
         }
     }
 
-    private boolean isValidInput(final String input) {
-        return "Rock".equals(input) || "Paper".equals(input) || "Scissors".equals(input);
+    private interface Selection {
+        public abstract boolean isValid();
+    }
+
+    private class Rock implements Selection {
+        @Override
+        public boolean isValid() {
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "Rock";
+        }
+    }
+
+    private class Paper implements Selection {
+        @Override
+        public boolean isValid() {
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "Paper";
+        }
+    }
+
+    private class Scissors implements Selection {
+        @Override
+        public boolean isValid() {
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "Scissors";
+        }
+    }
+
+    private class InvalidSelection implements Selection {
+        @Override
+        public boolean isValid() {
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return "Invalid Selection";
+        }
     }
 }
