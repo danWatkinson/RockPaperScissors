2996ee1a955e262e2146b540f9d1d171bef4c0fa
danWatkinson <dan.watkinson@gmail.com>
2014-12-17 16:39:35 +0000

    refactor: push the I/O stuff into HumanPlayer::makeSelection - it could still do with tidying but it's in the right place now

--- src/RockPaperScissorsGame.java
+++ src/RockPaperScissorsGame.java
@@ -1,16 +1,15 @@
+import player.HumanPlayer;
 import player.Player;
-import selection.InvalidSelection;
 import selection.Selection;
-import selection.SelectionBuilder;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 public class RockPaperScissorsGame {
     private final BufferedReader in;
     private final PrintStream out;
+    private Player player1;
     private Player player2;
 
     public static void main(String[] args) {
@@ -21,34 +20,15 @@ public class RockPaperScissorsGame {
     public RockPaperScissorsGame(final BufferedReader in, final PrintStream out) {
         this.in = in;
         this.out = out;
+        player1 = new HumanPlayer(in, out);
     }
 
     public void start() {
-        Selection playersSelection = getSelection();
+        Selection playersSelection = player1.makeSelection();
         out.println("You selected " + playersSelection);
         if (player2 != null) {
             player2.makeSelection();
         }
-    }
-
-    private Selection getSelection() { //TODO - this looks like something to pop out into another class, but I'm going to go ahead and
-                                       // look at the next acceptance criteria and see where that takes me, right now it's just one
-                                       // 'slightly rogue' method
-        out.println("Please select 'Rock', 'Paper', or 'Scissors'");
-        SelectionBuilder selectionBuilder = new SelectionBuilder();
-        Selection selection = new InvalidSelection();
-
-        while (!selection.isValid()) {
-            try {
-                selection = selectionBuilder.parse(in.readLine());
-                if (!selection.isValid()) {
-                    out.println("Sorry, I didn't recognise that. Please try again: Rock Paper or Scissors");
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to read input.", e);
-            }
-        }
-        return selection;
     }
 
     public void setPlayer2(final Player player2) {
---
+++ src/player/HumanPlayer.java
@@ -0,0 +1,38 @@
+package player;
+
+import selection.InvalidSelection;
+import selection.Selection;
+import selection.SelectionBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class HumanPlayer implements Player {
+
+    private final BufferedReader in;
+    private final PrintStream out;
+
+    public HumanPlayer(final BufferedReader in, final PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+
+    @Override
+    public Selection makeSelection() {
+        out.println("Please select 'Rock', 'Paper', or 'Scissors'");
+        SelectionBuilder selectionBuilder = new SelectionBuilder();
+        Selection selection = new InvalidSelection();
+
+        while (!selection.isValid()) {
+            try {
+                selection = selectionBuilder.parse(in.readLine());
+                if (!selection.isValid()) {
+                    out.println("Sorry, I didn't recognise that. Please try again: Rock Paper or Scissors");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to read input.", e);
+            }
+        }
+        return selection;
+    }}
